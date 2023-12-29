6f939e40630b9f5fed4d8d2585f9d72a6395ce3b
Dmitry <semiashkin@tut.by>
2023-12-29 11:53:57 +0300

    решено

---
+++ Zadacha_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца.
+//  Использовать рекурсию, не использвать циклы
+    
+ class Program
+ {
+    static void PrintArray(int[] myArray, int i = 0)
+    {
+        if (i < myArray.Length)
+           {
+           Console.WriteLine(myArray[i]);
+           
+           PrintArray(myArray,i+1);
+           }
+    }
+
+
+static void Main(string[] args)
+{
+    int[] myArray = { 5, 3, 2 };
+
+    PrintArray(myArray);
+    }
+
+
+ }
